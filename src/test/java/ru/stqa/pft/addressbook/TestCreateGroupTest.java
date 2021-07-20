c2e84fddb9cee0958ff21f0572a7869b613864a1
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-07-20 21:17:42 +0400

    создание контакта

--- src/test/java/ru/stqa/pft/addressbook/TestCreateGroupTest.java
+++ src/test/java/ru/stqa/pft/addressbook/TestCreateGroupTest.java
@@ -24,7 +24,7 @@ public class TestCreateGroupTest {
 
     private void login(String username, String password) {
         driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(778, 425));
+        driver.manage().window().setSize(new Dimension(1200, 1500));
         driver.findElement(By.name("user")).sendKeys(username);
         driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.name("pass")).sendKeys(Keys.ENTER);
