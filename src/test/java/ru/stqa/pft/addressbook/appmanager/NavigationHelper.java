b4d44005c9392238c832f523f04c3e7e536e5785
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-09-16 20:27:36 +0400

    l3_m9

--- src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,6 +10,18 @@ public class NavigationHelper extends GroupHelper{
     }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if(isElementPresent(By.tagName("h1"))
+                && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
+            click(By.linkText("groups"));
+    }
+
+    public void gotoHomePage(){
+        if(isElementPresent(By.id("maintable"))){
+            return;
+        }
+        click(By.linkText("home"));
     }
 }
