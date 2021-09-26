558d31271a1a50adcd7885885266157bb6e3d827
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-09-26 21:53:21 +0400

    l4_m9

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +15,20 @@ public class GroupCreationTest extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
         //int before = app.getGroupHelper().getGroupCount();
-        GroupData group = new GroupData("group1", null, null);
+        GroupData group = new GroupData("group2", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         //int after = app.getGroupHelper().getGroupCount();
         Assert.assertEquals(after.size(), before.size()+1);
 
-        int max = 0;
+      /*  int max = 0;
         for (GroupData g: after){
             if (g.getId()>max){
                 max = g.getId();
             }
-        }
-        group.setId(max);
+        }*/
+
+        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
