a8b9ff370378f321a35b750e397909cff02a8069
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-10-14 20:53:52 +0400

    l6_m7 Json

---
+++ src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,90 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if (format.equals("csv")) {
+            saveAsCsv(contacts, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(contacts, new File(file));
+        } else if (format.equals("json")) {
+            saveAsJson(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format" + format);
+        }
+    }
+
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create(); //красивое форматирование
+        String json = gson.toJson(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+    }
+
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsoluteFile());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getGroup()));
+                    //contact.getAllPhones(), contact.getHomePhone(), contact.getMobilePhone()));
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
+                    .withLastname(String.format("lastname %s", i)).withGroup(String.format("group %s", i)));
+                    //.withAllPhones(String.format("AllPhones %s", i)).withHomePhone(String.format("home %s", i)).withMobilePhone(String.format("mobile %s", i)));
+        }
+        return contacts;
+    }
+}
