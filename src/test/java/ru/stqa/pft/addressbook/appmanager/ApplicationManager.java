0f1a9e1652d35df2c4586366c3ecda62b1d8eb64
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-10-18 22:21:53 +0400

    l6_m10 config files

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,23 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver driver;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
@@ -21,28 +28,31 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         System.setProperty("webdriver.chrome.driver", "D:\\Autotests\\chromedriver_win32_93\\chromedriver.exe");
         System.setProperty("webdriver.firefox.marionette", "D:\\Autotests\\geckodriver-v0.29.1-win32\\geckodriver.exe");
         System.setProperty("webdriver.ie.driver", "D:\\Autotests\\IEDriverServer_Win32_3.150.2\\IEDriverServer.exe");
-        if (Objects.equals(browser, BrowserType.CHROME)){
+        if (Objects.equals(browser, BrowserType.CHROME)) {
             driver = new ChromeDriver();
         } else if (Objects.equals(browser, BrowserType.FIREFOX)) {
             driver = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.IE)){
+        } else if (Objects.equals(browser, BrowserType.IE)) {
             driver = new InternetExplorerDriver();
         }
         js = (JavascriptExecutor) driver;
         //driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/");
         driver.manage().window().setSize(new Dimension(1200, 1500));
+        driver.get(properties.getProperty("web.baseUrl"));
         GroupHelper = new GroupHelper(driver);
         contactHelper = new ContactHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,12 +5,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
